03f234b4b1902c1cfe34ae24b998f7148423ac2d
Cristian <desancheztorres@gmail.com>
2020-12-14 08:46:35 +0000

    Models and Admin, Views and Templates and Todo List View

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Todo
 
-# Create your views here.
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    context = {
+        "todo_list": todos
+    }
+    return render(request, "todo/todo_list.html", todos)
